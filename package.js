37f02cbb5d9c0d46156b3571778401b618e49d1b
igps <polaristele@gmail.com>
2015-02-06 21:55:14 +0800

    bump version number to the same version as npm package

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Let you work with some GPS trackers that connects through tcp. Also saves the data on your mongodb database ;)",
-  version: '0.2.0',
+  version: '0.2.5',
   name: "solo999:gps-tracking",
   git: "https://github.com/solo999/meteor-gps-tracking"
 });
