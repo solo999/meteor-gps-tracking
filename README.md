acea7393b28d00732c4fc2a88dbafa65c0c5fd8e
igps <polaristele@gmail.com>
2015-02-06 21:16:07 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Wrapper for https://www.npmjs.org/package/gps-tracking
---
+++ package.js
@@ -0,0 +1,21 @@
+Package.describe({
+  summary: "Let you work with some GPS trackers that connects through tcp. Also saves the data on your mongodb database ;)",
+  version: '0.2.5',
+  name: "solo999:gps-tracking",
+  git: "https://github.com/solo999/meteor-gps-tracking"
+});
+
+Npm.depends({
+  gps-tracking: "0.2.5", 
+});
+
+Package.onUse(function (api) {
+
+  api.add_files([
+    'lib/gps-tracking.js',
+  ], ['server']);
+  
+  api.export([
+    'gps'
+  ], ['server']);
+});
